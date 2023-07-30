5ccb62bdeacab6851f2c6524a1cb638a11c119cf
Ivan Platonov <EventGraphDev@gmail.com>
2023-07-30 04:26:39 +0300

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+IWebHostEnvironment env = app.Environment;
+if(env.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Hello Wordl!");
+    });
+});
+
+app.Run();
